b84449a696b49ae2dfa3f15020d6207635d3fc77
Diego Abade <63867917+AbadeDiego@users.noreply.github.com>
2023-09-09 17:09:17 -0300

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-fv9@cv&b0toqy0ujxy$zl!7t$xssq#w*gs^ee5!8gmx)d)299&
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['http://', 'https://']
+CSRF_TRUSTED_ORIGINS = ['http://bodetech.blog.br', 'https://bodetech.blog.br']
 
 
 # Application definition
